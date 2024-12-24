e7ac6fffc43d9a2fa5346d11133787af2517d47a
JI1IN <1918jas@gmail.com>
2024-12-24 16:38:00 +0100

    Fixed "Could Not Proxy Request from localhost:3000 to localhost:5000"

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import json
 from datetime import datetime
 
 app = Flask(__name__)
+CORS(app)
 
 class TaskTracker:
     def __init__(self):
